853f2b2efc5ca67619af4891d5e8c6e6dacda152
Michal <michal.jan.sypek@gmail.com>
2023-10-31 15:50:49 +0100

    Lab C

--- src/Controller/ForecastController.php
+++ src/Controller/ForecastController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\City;
 use App\Repository\ForecastRepository;
 
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +13,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class ForecastController extends AbstractController
 {
 //    #[Route('/forecast/{id}', name: 'app_city', condition: "params['id']>=0")]
-    #[Route('/forecast/{id}', name: 'app_city', requirements: ['id' => '\d+'])]
-    public function index(City $city,ForecastRepository $repository): Response
+//    #[Route('/forecast/{id}', name: 'app_city', requirements: ['id' => '\d+'])]
+//    public function by_id(City $city,ForecastRepository $repository): Response
+//    {
+//        $forecasts = $repository->findByCity($city);
+//
+//
+//        return $this->render('forecast/city.html.twig', [
+//            'controller_name' => 'ForecastController',
+//            'forecasts' => $forecasts,
+//            'city' => $city,
+//        ]);
+//    }
+
+    #[Route('/forecast/{name}', name: 'app_city', defaults: ["name"=>"Szczecin"])]
+    public function index(
+        #[MapEntity(mapping: ['name' => 'name'])] City $city,
+        ForecastRepository $repository
+    ): Response
     {
         $forecasts = $repository->findByCity($city);
 
